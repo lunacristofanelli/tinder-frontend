fed803875f30fa158176f3de53d2864fa321803b
Luna <lunacristofanelli@gmail.com>
2024-05-22 23:52:46 -0300

    Realizado parte del front del home y backend

---
+++ src/app/componentes/TextoInicio/textoInicio.tsx
@@ -0,0 +1,11 @@
+import './textoInicio.css'
+
+
+export const TextoInicio = () => {
+  return (
+    <div className='containerText'>
+      <h1>Encuentra a tu Crush en los lugares que amas.</h1>
+      <h2>Hazlo realidad.</h2>
+    </div>
+  )
+}
